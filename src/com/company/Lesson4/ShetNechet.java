147a402da53509e7609381917cca9afe01718413
Cub86 <tadjiketm@gmail.com>
2019-03-13 01:11:48 +0200

    Залита 1 версия

---
+++ src/com/company/Lesson4/ShetNechet.java
@@ -0,0 +1,21 @@
+package com.company.Lesson4;
+
+import java.util.Scanner;
+
+public class ShetNechet {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // создаём Scanner
+        int i;
+        System.out.print("Введите целое число: ");
+        if (sc.hasNextInt()) { // возвращает если целое число
+            i = sc.nextInt(); // считываю целое число
+            if (i % 2 == 0) {
+                System.out.println("Число" + " " + i + " " + "четное");
+            } else {
+                System.out.println("Вы ввели не четное целое число");
+            }
+        }
+    }
+}
+
