4dc0c121e54a1c85838cf87cb45e30847e7505e2
Cub86 <tadjiketm@gmail.com>
2019-03-16 22:23:57 +0200

    Залита 3 версия. Вынесены повторения в отдельный метод командой ctrl+alt+m

--- src/com/company/lesson4/SredneeZnachenie.java
+++ src/com/company/lesson4/SredneeZnachenie.java
@@ -8,12 +8,10 @@ public class SredneeZnachenie {
 
         Scanner in = new Scanner(System.in);
         double a, b, c;
-        System.out.println("Введите Первое число ");
-        a = in.nextDouble();
-        System.out.println("Введите второе число ");
-        b = in.nextDouble();
-        System.out.println("Введите третье число");
-        c = in.nextDouble();
+        PovtorMetod povtorMetod = new PovtorMetod(in).invoke();
+        a = povtorMetod.getA();
+        b = povtorMetod.getB();
+        c = povtorMetod.getC();
 
         String str1 = "13.7";
         String str2 = "5.1";
@@ -38,5 +36,38 @@ public class SredneeZnachenie {
         }
         double srednee = (a + b + c) / 3;
         System.out.println("Среднее арифметическое= " + srednee);
+    }
+
+    private static class PovtorMetod {
+        private Scanner in;
+        private double a;
+        private double b;
+        private double c;
+
+        public PovtorMetod(Scanner in) {
+            this.in = in;
+        }
+
+        public double getA() {
+            return a;
+        }
+
+        public double getB() {
+            return b;
+        }
+
+        public double getC() {
+            return c;
+        }
+
+        public PovtorMetod invoke() {
+            System.out.println("Введите Первое число ");
+            a = in.nextDouble();
+            System.out.println("Введите второе число ");
+            b = in.nextDouble();
+            System.out.println("Введите третье число");
+            c = in.nextDouble();
+            return this;
+        }
     }
 }
---
+++ src/com/company/lesson7/Phone.java
@@ -0,0 +1,12 @@
+package com.company.lesson7;
+
+public class Phone {
+    private String number, model;
+    private int weight;
+
+
+
+
+
+    }
+}
