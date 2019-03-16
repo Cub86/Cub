1e04eaa7a1de7516ec1fdd86f454d038a7c2bde9
Cub86 <tadjiketm@gmail.com>
2019-03-17 01:21:36 +0200

    Версия 2

--- src/com/company/lesson7/PhoneZad5.java
+++ src/com/company/lesson7/PhoneZad5.java
@@ -1,8 +1,9 @@
 package com.company.lesson7;
 
 public class PhoneZad5 {
-    String number, model;
-    int weight;
+    private static int count;
+    private String number, model;
+    private int weight;
 
     PhoneZad5(String number, String model, int weight) {
         this(number, model);
@@ -12,19 +13,63 @@ public class PhoneZad5 {
     PhoneZad5(String number, String model) {
         this.number = number;
         this.model = model;
+        count++;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 
     void receiveCall(String name) {
         System.out.println("Звонит " + name);
     }
 
-    void receiveCall(String name, String number) {
-        System.out.println("Звонит " + name + "с номером телефона" + number);
+    public static int getCount() {
+        return count;
     }
+
+    void receiveCall(String name, String number) {
+        System.out.println("Мой номер телефона " + this.number);
+        System.out.println("Звонит " + name + " с номера телефона" + number);
+    }
+
+    void sendMessage(String... numbers) {
+        System.out.println("Отправляю сообщение номерам: ");
+        for (String number : numbers) {
+            System.out.println(number + " ");
+            System.out.println();
+        }
+    }
+
+    ;
 
     public static void main(String[] args) {
         PhoneZad5 phone = new PhoneZad5("+380971135910", "Nokia", 6);
-        phone.receiveCall("Мама", "+380634708370");
+        phone.receiveCall("Мама", " +380634708370");
+        phone.sendMessage("0678575555", "0735966896");
+        phone.sendMessage("0638523551");
+
+        System.out.println(getCount());
     }
 }
 
