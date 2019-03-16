fcada2ae4cfc15f7e287fc406eb04d09af8adb4e
Cub86 <tadjiketm@gmail.com>
2019-03-16 23:52:05 +0200

    Версия 1

---
+++ src/com/company/lesson6/Phone.java
@@ -0,0 +1,7 @@
+package com.company.lesson6;
+
+public class Phone {
+    String number, model;
+    double weight;
+
+       }
---
+++ src/com/company/lesson6/PhoneDemo.java
@@ -0,0 +1,26 @@
+package com.company.lesson6;
+
+public class PhoneDemo {
+    public static void main(String[] args) {
+        Phone phone1 = new Phone();
+        Phone phone2 = new Phone();
+        Phone phone3 = new Phone();
+
+        phone1.number = "+380971135910";
+        phone1.model = "Apple";
+        phone1.weight = 4.5;
+
+        phone2.number = "+380634708370";
+        phone2.model = "Samsung";
+        phone2.weight = 5.0;
+
+        phone3.number = "+380671431879";
+        phone3.model = "Lenovo";
+        phone3.weight = 5.5;
+
+        System.out.println(phone1.number + " " + phone1.model + " " + phone1.weight);
+        System.out.println(phone2.number + " " + phone2.model + " " + phone2.weight);
+        System.out.println(phone3.number + " " + phone3.model + " " + phone3.weight);
+
+    }
+}
