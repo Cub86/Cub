f25034c5c3524e4eb6cc2bddd0b1568ee77d446b
Cub86 <tadjiketm@gmail.com>
2019-03-17 01:21:20 +0200

    Версия 2

--- src/com/company/lesson6/Phone.java
+++ src/com/company/lesson6/Phone.java
@@ -1,7 +1,30 @@
 package com.company.lesson6;
 
 public class Phone {
+    private static int count;
     String number, model;
     double weight;
 
-       }
+    Phone(String n, String m, double w) {
+        number = n;
+        model = m;
+        weight = w;
+    }
+
+    Phone(String n, String m) {
+        number = n;
+        model = m;
+        count++;
+    }
+
+    Phone() {
+    }
+
+    void receineCall(String name) {
+        System.out.println("Имя звонящего" + name);
+    }
+
+    String getNumber() {
+        return number;
+    }
+}
