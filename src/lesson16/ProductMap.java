5b629bc510a0d49384c2b84d160e37f089143c01
student <fyhgf@dfdf.bhm>
2019-04-21 11:30:05 +0300

    Версия 1

---
+++ src/lesson16/ProductMap.java
@@ -0,0 +1,44 @@
+package lesson16;
+
+/*import collections.Product;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ProductMap {
+    public static void main(String[] args) {
+        Map<String, Product> map = new HashMap<>();
+        map.put("Кукла", new Product("Кукла", 450, Ukraine));
+        map.put("Кубик", new Product("Кубик", 50, Ukraine));
+        map.put("Машинка", new Product("Машинка", 150, Ukraine));
+
+        enuverateKeys(map);
+        enumerateValues(map);
+        enumerateEntries(map);
+
+        map.forEach(k, v)->System.out.println("Ключ" + k + ",Значение" + v);
+    }
+
+    public static void enumerateValues(Map<String,Prduct> map) {
+        System.out.println("Ключи");
+        Collection<Product> values = map.values();
+        values.forEach( System.out.println);
+
+    }
+        Set<Map.Entry<String, Prduct>> entrySet = map.entrySet();
+        for (Map.Entry<String, Product> entry : entrySet) {
+            System.out.println("Ключ" + entry.getKey() + "Значение:" + entry.getValue());
+        }
+        Collection<Product> ;
+
+    }
+
+    public static void enumerateKeys(Map String[] args) {
+
+    }
+}
+*/
+
+
