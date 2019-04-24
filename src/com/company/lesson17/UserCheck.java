6819b7194ed1db48722622a9f436b66c0f015729
Oleg Topchiy <olegtopchiy93@gmail.com>
2019-04-24 21:18:15 +0300

    Версия 1

---
+++ src/com/company/lesson17/UserCheck.java
@@ -0,0 +1,18 @@
+package com.company.lesson17;
+
+/*public static void main(String[]args){
+      }
+
+public static boolean verify(String login,String password,String confiPassword){
+        if(login.lenght()>20){
+        throw new WrongLoginException("Login.lenght>20");
+        }
+        if(passwold.lenght()>20){
+        throw new WrongLoginException | WrongLoginException ("Password.lenght>20");
+       s }
+        } catch (WrongLoginException e)
+
+finally {
+    Sistem.out.printf ("Login:")
+        }
+*/
